3cb729b230998a24870322aded0f783f33e6cd20
Carolyn Wang <carolynw@fb.com>
2022-06-06 11:53:52 -0700

    fix color swapping error

--- app/src/main/java/com/codepath/debuggingchallenges/activities/ChangeBackgroundActivity.java
+++ app/src/main/java/com/codepath/debuggingchallenges/activities/ChangeBackgroundActivity.java
@@ -3,6 +3,8 @@ package com.codepath.debuggingchallenges.activities;
 import android.graphics.Color;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
 import android.view.View;
 
 import com.codepath.debuggingchallenges.R;
@@ -18,6 +20,7 @@ public class ChangeBackgroundActivity extends AppCompatActivity {
     }
 
     public void onGoClick(View view) {
+//        Log.println(0, "button","clicking");
         View mainView = findViewById(android.R.id.content);
         mainView.setBackgroundColor(getNextColor());
     }
